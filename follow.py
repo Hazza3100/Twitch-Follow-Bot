67963f7b4d8933d32e516caa927815c9bf19616e
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-08-16 14:12:40 +0100

    Update follow.py

--- follow.py
+++ follow.py
@@ -43,29 +43,29 @@ def follow():
         token = random.choice(tokens)
         
         headers = {
-        'Accept': '*/*',
-        'Accept-Language': 'en-GB',
-        'Authorization': f'OAuth {token}',
-        'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-        'Connection': 'keep-alive',
-        'Content-Type': 'text/plain;charset=UTF-8',
-        'Origin': 'https://www.twitch.tv',
-        'Referer': 'https://www.twitch.tv/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-    }
-
-    data = '[{"operationName":"FollowButton_FollowUser","variables":{"input":{"disableNotifications":false,"targetID":"'+channel_ID+'"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"800e7346bdf7e5278a3c1d3f21b2b56e2639928f86815677a7126b093b2fdd08"}}}]'
-    r = requests.post('https://gql.twitch.tv/gql', headers=headers, data=data)
-    if r.status_code == 200:
-        print(f"{Fore.GREEN}Followed {Fore.RESET}{channel_name}{Fore.RESET}\n")
-    else:
-        print(F"{Fore.RED}Error{Fore.RESET}\n")
+            'Accept': '*/*',
+            'Accept-Language': 'en-GB',
+            'Authorization': f'OAuth {token}',
+            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+            'Connection': 'keep-alive',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Origin': 'https://www.twitch.tv',
+            'Referer': 'https://www.twitch.tv/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            }
+        
+        data = '[{"operationName":"FollowButton_FollowUser","variables":{"input":{"disableNotifications":false,"targetID":"'+channel_ID+'"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"800e7346bdf7e5278a3c1d3f21b2b56e2639928f86815677a7126b093b2fdd08"}}}]'
+        r = requests.post('https://gql.twitch.tv/gql', headers=headers, data=data)
+        if r.status_code == 200:
+            print(f"{Fore.GREEN}Followed {Fore.RESET}{channel_name}{Fore.RESET}\n")
+        else:
+            print(F"{Fore.RED}Error{Fore.RESET}\n")
 
 
 
