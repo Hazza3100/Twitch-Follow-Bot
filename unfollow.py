73db963be402dd225657c44880c99a1d472be5a6
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-08-16 14:12:53 +0100

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -37,40 +37,36 @@ channel_ID = get_username()
 
 
 
-
-
-
 def unfollow():
+    
+    for i in range(7):
+        tokens = open('tokens.txt', 'r').read().splitlines()
+        token = random.choice(tokens)
+        
+        headers = {
+            'Accept': '*/*',
+            'Accept-Language': 'en-GB',
+            'Authorization': f'OAuth {token}',
+            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+            'Connection': 'keep-alive',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Origin': 'https://www.twitch.tv',
+            'Referer': 'https://www.twitch.tv/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            }
 
-
-    tokensf = open("tokens.txt")
-    tokens = random.choice(tokensf.read().splitlines())
-    tokensf.close()
-
-    headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-GB',
-    'Authorization': f'OAuth {tokens}',
-    'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-    'Client-Session-Id': 'aba52d30ca5f58e2',
-    'Client-Version': '355d123b-5b7b-4e60-b5b3-e998df86e7c1',
-    'Connection': 'keep-alive',
-    'Content-Type': 'text/plain;charset=UTF-8',
-    'Origin': 'https://www.twitch.tv',
-    'Referer': 'https://www.twitch.tv/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-    'X-Device-Id': '9e7GxupzurGeVtrQDDaTEzkOnXjijE7l',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    }
-
-    data = '[{"operationName":"FollowButton_UnfollowUser","variables":{"input":{"targetID":"'+channel_ID+'"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"f7dae976ebf41c755ae2d758546bfd176b4eeb856656098bb40e0a672ca0d880"}}}]'
-    r = requests.post('https://gql.twitch.tv/gql', headers=headers, data=data)
-    print(Fore.GREEN + f"Unfollowed {Fore.RESET}{channel_name}{Fore.RESET}\n")
+        data = '[{"operationName":"FollowButton_UnfollowUser","variables":{"input":{"targetID":"'+channel_ID+'"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"f7dae976ebf41c755ae2d758546bfd176b4eeb856656098bb40e0a672ca0d880"}}}]'
+        r = requests.post('https://gql.twitch.tv/gql', headers=headers, data=data)
+        if r.status_code == 200:
+            print(f"{Fore.GREEN}Followed {Fore.RESET}{channel_name}{Fore.RESET}\n")
+        else:
+            print(F"{Fore.RED}Error{Fore.RESET}\n")
 
 
 
