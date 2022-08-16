975d7d9136b106f054c856159a8b8643e31ecee4
Hazza <86918289+Hazza3100@users.noreply.github.com>
2022-08-16 14:10:55 +0100

    Update follow.py

--- follow.py
+++ follow.py
@@ -38,18 +38,15 @@ channel_ID = get_username()
 
 def follow():
 
-    tokensf = open("tokens.txt")
-    tokens = random.choice(tokensf.read().splitlines())
-    tokensf.close()
-
-
-    headers = {
+    for i in range(7):
+        tokens = open('tokens.txt', 'r').read().splitlines()
+        token = random.choice(tokens)
+        
+        headers = {
         'Accept': '*/*',
         'Accept-Language': 'en-GB',
-        'Authorization': f'OAuth {tokens}',
+        'Authorization': f'OAuth {token}',
         'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
-        'Client-Session-Id': 'aba52d30ca5f58e2',
-        'Client-Version': '355d123b-5b7b-4e60-b5b3-e998df86e7c1',
         'Connection': 'keep-alive',
         'Content-Type': 'text/plain;charset=UTF-8',
         'Origin': 'https://www.twitch.tv',
@@ -58,7 +55,6 @@ def follow():
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-site',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-        'X-Device-Id': '9e7GxupzurGeVtrQDDaTEzkOnXjijE7l',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -66,7 +62,10 @@ def follow():
 
     data = '[{"operationName":"FollowButton_FollowUser","variables":{"input":{"disableNotifications":false,"targetID":"'+channel_ID+'"}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"800e7346bdf7e5278a3c1d3f21b2b56e2639928f86815677a7126b093b2fdd08"}}}]'
     r = requests.post('https://gql.twitch.tv/gql', headers=headers, data=data)
-    print(Fore.GREEN + f"Followed {Fore.RESET}{channel_name}{Fore.RESET}\n")
+    if r.status_code == 200:
+        print(f"{Fore.GREEN}Followed {Fore.RESET}{channel_name}{Fore.RESET}\n")
+    else:
+        print(F"{Fore.RED}Error{Fore.RESET}\n")
 
 
 
